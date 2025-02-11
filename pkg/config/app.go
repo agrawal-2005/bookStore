35f501c5d2aff6c5d49465ebb86f3a1e3136a272
Prashant Agrawal <agrawalprashant906@gmail.com>
2025-02-12 04:12:16 +0530

    Adding CURD Operations

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -13,7 +13,6 @@ var (
 func ConnectDB() {
 	dsn := "prashant:GoyalPiyush123@tcp(127.0.0.1:3306)/bookstore?charset=utf8mb4&parseTime=True&loc=Local"
 
-	// Open the database connection
 	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
