ed3924353bdddccfb253f3cb5135c75a221fbb36
Prashant Agrawal <agrawalprashant906@gmail.com>
2025-02-12 04:27:31 +0530

    Added .gitignore & Update app.go to hide sensitive files

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,17 +1,28 @@
 package config
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"log"
 )
 
-var (
-	db *gorm.DB
-)
+var db *gorm.DB
 
 func ConnectDB() {
-	dsn := "prashant:GoyalPiyush123@tcp(127.0.0.1:3306)/bookstore?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	database := os.Getenv("DB_NAME")
+
+	dsn := username + ":" + password + "@tcp(" + host + ")/" + database + "?charset=utf8mb4&parseTime=True&loc=Local"
 
 	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
